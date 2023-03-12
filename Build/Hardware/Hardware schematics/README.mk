85bbae7cdb251c0d1ea99f50c0f21687462ed295
internetoftobi <oyinlolaoluwatobi@gmail.com>
2023-03-12 18:18:43 -0400

    Added BoM link

--- Build/Hardware/Hardware schematics/README.mk
+++ Build/Hardware/Hardware schematics/README.mk
@@ -1,3 +1,3 @@
 Files for Electronic production
 
-A BoM (bill of materials) listing all the required parts and components to build a Flatburn hardware can be found [TOBI ADDS LINK]
+A BoM (bill of materials) listing all the required parts and components to build a Flatburn hardware can be found [here](https://docs.google.com/spreadsheets/d/1-fR-0hTxHKbjaRf8DbH62WgUFVeNE4eUEsaAd-YdDYg/edit?usp=sharing)
