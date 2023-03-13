3a6df19ca97046a1eae34073fc06968919e02bc8
Sanjana P <31711564+lightbulbnerd@users.noreply.github.com>
2023-03-13 09:13:14 -0400

    Update README.mk

--- Learn/Facilitator Handbook/README.mk
+++ Learn/Facilitator Handbook/README.mk
@@ -1,1 +1,12 @@
-Please explain what's in this folder. How the notebooks can be used. Borrow text from the coal cities paper. Thank you.
+This folder contains support files for the python coding activity 
+
+The coding activity presented students with
+mobile air quality monitoring data and guided them through the process of
+plotting time series and creating maps of of various parameters, including
+temperature, humidity, and PM2.5.
+The coding activity began by asking students to set up their coding en-
+vironment, importing several python libraries, reading in the relevant data,
+which was provided in a csv (comma-separated values) format, and setting
+the local timezone. They were then asked to perform basic data cleaning
+tasks, removing outlier values of temperature, humidity, and particulate mat-
+ter data
