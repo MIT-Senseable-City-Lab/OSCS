611b563ed6ad0f4e82a6604ed93fea29dcd17563
Sanjana P <31711564+lightbulbnerd@users.noreply.github.com>
2023-03-13 09:10:00 -0400

    Update README.mk

--- Learn/Coding Exercise/README.mk
+++ Learn/Coding Exercise/README.mk
@@ -1,1 +1,4 @@
-Please describe files in this folder
+This folder contains a coding activity written in Python to introduce users to air quality and environmental sensing methods
+by working with CityScanner Data, exploring time series analysis, geospatial analysis, and pollution hotspot analysis.
+There are 2 versions - one file containing just the python code and some descriptive text, and one containing a Google 
+CoLab file.
