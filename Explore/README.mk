c6a27ed0aa0a355c7b95e7f8e2e0362cd9260e65
AnduinWang <anwang0819@icloud.com>
2023-03-13 14:14:36 +0000

    Update README.mk

--- Explore/README.mk
+++ Explore/README.mk
@@ -1,1 +1,4 @@
-Please describe this directory contents
+# EXPLORE OSCS
+The Explore folder contains two sections:
+- [/datasets](https://github.com/MIT-Senseable-City-Lab/OSCS/tree/main/Explore/Datasets): The folder contains `.csv` Calibrated air quality data of the the cityscanner. 
+- [/data collection document](https://github.com/MIT-Senseable-City-Lab/OSCS/tree/main/Explore/Data%20collection%20document): Open-source manual and code for data collection
