36402fa3df0f652f323910d6b20241359c21a48f
Sanjana P <31711564+lightbulbnerd@users.noreply.github.com>
2023-03-13 09:20:50 -0400

    Update README.mk

--- Learn/Facilitator Handbook/README.mk
+++ Learn/Facilitator Handbook/README.mk
@@ -1,12 +1,5 @@
-This folder contains support files for the python coding activity 
-
-The coding activity presented students with
-mobile air quality monitoring data and guided them through the process of
-plotting time series and creating maps of of various parameters, including
-temperature, humidity, and PM2.5.
-The coding activity began by asking students to set up their coding en-
-vironment, importing several python libraries, reading in the relevant data,
-which was provided in a csv (comma-separated values) format, and setting
-the local timezone. They were then asked to perform basic data cleaning
-tasks, removing outlier values of temperature, humidity, and particulate mat-
-ter data
+This folder contains support files for the python coding activity, aiming to provide users with background information
+and context on mobile air quality monitoring and environmental sensing. There are 2 files - a facilitator guide and
+workshop slides. The facilitator guide is meant to support an individual or teaching team in combining these resources
+to deliver during a class or workshop. The workshop slides are an educational presentation on the background of 
+environmental sensing, mobile air quality monitoring, and CityScanner. 
