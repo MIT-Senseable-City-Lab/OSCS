54d0a12d5f70666606f1a129e2ba2ccf87c81bbf
AnduinWang <anwang0819@icloud.com>
2023-03-13 13:43:18 +0000

    Update README.mk

--- Explore/Datasets/README.mk
+++ Explore/Datasets/README.mk
@@ -1,1 +1,12 @@
 Please desribe directory content, including a table with list for each pilot: type of data collected, location, pilot start-end.
+# Open City Scanner Data
+This folder contains open City Scanner data sets collected in four deployments in three cities. Raw sensor readings were collected and calibrated following standardized air sensor performance evaluation guidelines recommended by the U.S. EPA. The general method is presented in the following diagram and details can be found in the open source calibration manual.
+
+
+Figure. Data post-processing and validation protocols
+
+Detailed information about the deployments and data files is included in he following table.
+
+Table. Deployments and open data sets
+
+Python scripts for data calibration is included in the Calibration Codes folder.
